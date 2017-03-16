550e78b245410356cbbd1b20b32d4f01d47f9dd9
James Kulon <james.kulon@gmail.com>
2017-03-16 15:17:48 -0700

    initial

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoshDarnYuck
+{
+    public class Cleaner
+    {
+        public Dictionary<string, string> words { get; set; }
+
+        public string Clean(string sentence)
+        {
+            string[] strArr = sentence.Split(' ');
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < strArr.Length; i++)
+            {
+                if (words.ContainsKey(strArr[i].ToLower()))
+                {
+                    strArr[i] = words[strArr[i].ToLower()];
+                }
+                sb.Append(strArr[i] + " ");
+            }
+            return sb.ToString();
+        }
+
+
+        public Cleaner()
+        {
+            words.Add("darn", "d**n");
+            words.Add("gosh", "g**h");
+            words.Add("yuck", "y**k");
+
+        }
+
+
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
